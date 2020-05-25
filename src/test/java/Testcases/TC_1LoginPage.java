ab208c286cd84a64be634ec1bd95f1ec3f4acb79
ramansubramani <ramansubramani65@gmail.com>
2020-05-25 06:04:19 -0700

    Peace

--- src/test/java/Testcases/TC_1LoginPage.java
+++ src/test/java/Testcases/TC_1LoginPage.java
@@ -8,10 +8,10 @@ public class TC_1LoginPage {
 	public void toCheckTheStatus() {
 		for (int i = 0; i < 5; i++) {
 			System.out.println("Welcome To World");	
-		}
-		
+		}	
 for (int i = 0; i < 2; i++) {
-	System.out.println("pursuit of happiness");
+	System.out.println("pursuit of Peace");
+	
 }
 		
 		
